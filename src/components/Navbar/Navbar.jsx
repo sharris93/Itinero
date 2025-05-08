b6f70c9ba2cf7bab9fe000c9ed59003e5ba507b2
Sam <sharrisdev@gmail.com>
2025-05-08 16:27:31 +0100

    added sign out functionality

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,45 @@
 import { NavLink } from "react-router"
 import './Navbar.css'
+import { useContext } from "react"
+import { UserContext } from "../../contexts/UserContext"
+import { removeToken } from "../../utils/auth"
 
 export default function Navbar(){
+  // * Context
+  const { user, setUser } = useContext(UserContext)
+
+  const handleSignOut = () => {
+    removeToken()
+    setUser(null)
+  }
+
   return (
     <header>
       <div className="brand-logo">
         <NavLink to="/">🌍</NavLink>
       </div>
       <nav>
-        <NavLink to="/activities/new">Create activity</NavLink>
         <NavLink to="/activities">Activities</NavLink>
-        <NavLink to="/register">Register</NavLink>
-        <NavLink to="/login">Login</NavLink>
+
+        { user 
+          ? (
+            <>
+              {/* Signed in routes */}
+              <NavLink to="/activities/new">Create activity</NavLink>
+              <NavLink onClick={handleSignOut} to="/login">Sign out</NavLink>
+            </>
+          )
+          : (
+            <>
+              {/* Signed out routes */}
+              <NavLink to="/register">Register</NavLink>
+              <NavLink to="/login">Login</NavLink>
+            </>
+          )
+        }
+        
+
+        
       </nav>
     </header>
   )
